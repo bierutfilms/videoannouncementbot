af41a0bd507b1337659b0bdc831da07d9e4c8824
bierutfilms <drew@bierutfilms.com>
2025-01-24 16:58:12 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 // Replace these with your actual tokens and IDs
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
-const YOUTUBE_CHANNEL_ID = "UCYHad4wPZcAqnVtH9wSbA8g"; // YouTube channel ID to monitor
+const YOUTUBE_CHANNEL_ID = "UCQANb2YPwAtK-IQJrLaaUFw"; // YouTube channel ID to monitor
 
 // Mapping keywords to Discord channel IDs
 const CHANNEL_KEYWORDS = {
